e30306a226eb8d816a1298bc840456f4d6ffe333
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-10-03 00:44:23 +0300

    upload on vercel

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,16 +1,38 @@
-const multer = require("multer");
-const path = require("path");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./upload");
-  },
+// const multer = require("multer");
+// const path = require("path");
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./upload");
+//   },
 
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + "-" + uniqueName + ext);
+//   filename: (req, file, cb) => {
+//     const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     const ext = path.extname(file.originalname);
+//     cb(null, file.fieldname + "-" + uniqueName + ext);
+//   },
+// });
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+require("dotenv").config();
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "doctors", // هيتخزن جوه فولدر اسمه doctors
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
+
 const upload = multer({ storage: storage });
 
 module.exports = upload;
